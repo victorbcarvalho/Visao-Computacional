60cbbc3f2a32de8e2444c51f623470369397ea5f
victorbcarvalho <victor.carvalhobastos@gmail.com>
2023-02-16 21:50:49 -0300

    Parametro scaleFactor eliminando os falsos positivos

--- deteccao-faces/detector-opencv.py
+++ deteccao-faces/detector-opencv.py
@@ -1,36 +1,19 @@
 import cv2 # OpenCV
 
-imagem = cv2.imread('../Images/people1.jpg')
-
-imagem.shape
-
-window_name = 'image'
-
 def imshow(window_name, imagem):
     cv2.imshow(window_name, imagem)
     cv2.waitKey(0)
 
+window_name = 'image'
+
+imagem = cv2.imread('../Images/people1.jpg')
 # diminuindo o tamanho da imagem
 imagem = cv2.resize(imagem, (800,600))
-
-imshow(window_name, imagem)
-
 # Convertendo para escala de cinza
 imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-
-imshow(window_name, imagem_cinza)
-
 # Detecção de faces
-detector_faces = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-''' O openCV já disponibiliza um modelo treinado '''
-
-deteccoes = detector_faces.detectMultiScale(imagem_cinza)
-
-deteccoes # cada linha indica uma face identificada
-
-len(deteccoes)
-
-for x, y, w, h in deteccoes:
-    cv2.rectangle(imagem, (x,y), (x+w, y+h), (0,255,255), 5)
-
+detector_faces = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml') # O openCV já disponibiliza um modelo treinado
+deteccoes = detector_faces.detectMultiScale(imagem_cinza, scaleFactor=1.09) # cada linha indica uma face identificada
+for (x, y, w, h) in deteccoes:
+    cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 5)
 imshow(window_name, imagem)
