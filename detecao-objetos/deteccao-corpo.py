919b95d4f8ac2d315948ea7405873fb03291868d
victorbcarvalho <victor.carvalhobastos@gmail.com>
2023-02-27 21:03:26 -0300

    Alteracao do detector de corpo

--- detecao-objetos/deteccao-corpo.py
+++ detecao-objetos/deteccao-corpo.py
@@ -3,7 +3,7 @@ import cv2
 detector_corpo = cv2.CascadeClassifier('H:/Users/basto/Documents/visao-computacional/Cascades/fullbody.xml')
 
 imagem = cv2.imread('H:/Users/basto/Documents/visao-computacional/Images/people3.jpg')
-imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BRG2GRAY)
+imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 deteccoes = detector_corpo.detectMultiScale(imagem_cinza, scaleFactor=1.05, minNeighbors=5, minSize=(50, 50))
 
